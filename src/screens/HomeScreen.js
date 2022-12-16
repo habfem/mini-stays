44ab027c664a36210913ceffc70d43bb33493207
habfem <habeebmustapha96@gmail.com>
2022-12-16 14:45:09 -0600

    Home

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductCarousel from '../components/ProductCarousel'
+import Offers from '../components/Offers'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <ProductCarousel />
+      <hr></hr>
+      <Offers />
+    </>
+  )
+}
+
+export default HomeScreen
